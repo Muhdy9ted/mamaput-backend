ec325dcce570479094248bad972ae3f6b90b9343
Muhammed Abdullahi <muhammed.abdullahi@swisscom.com>
2025-03-10 10:28:11 +0100

    started drink model

---
+++ src/domain/interfaces/Drink.ts
@@ -0,0 +1,10 @@
+export interface Drink {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    image: string;
+    category: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
